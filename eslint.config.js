34624d042971a37a396a6f59c71b82a2463b0bc9
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2024-12-18 01:18:39 -0300

    teste 5

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,28 @@
 // eslint.config.js
-module.exports = {
+import { defineConfig } from 'eslint';
+
+export default defineConfig({
+  languageOptions: {
     parserOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
     },
-    env: {
-      browser: true,
-      node: true,
-    },
-    extends: ['eslint:recommended'],
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx'], // Se estiver usando TypeScript
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      languageOptions: {
         parser: '@typescript-eslint/parser',
-        extends: [
-          'eslint:recommended',
-          'plugin:@typescript-eslint/recommended',
-        ],
       },
-    ],
-  };
-  
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+    },
+  ],
+});
